05252cf666b407c5427a921190a36feb45d48ccf
Trisna Aprilia <144975702+trisna-aprilia@users.noreply.github.com>
2023-09-26 06:35:48 +0800

    Update posttest.py

--- posttest-1/posttest.py
+++ posttest-1/posttest.py
@@ -6,23 +6,27 @@ kelas = (input("kelas :"))
 password = (input("password :"))
 print("=========================")
 
-print("PROGRAM KONVERSI SATUAN MASSA KILOGRAM")
-# input berat
-kg = float(input("Masukkan nilai dalam kilogram: "))
-pilihan = input("Masukkan pilihan satuan massa (1/2/3): ")
+print("======konversi satuan massa kilogram=====")
+# input massa
+kilogram = float(input("masukkan kilogram:"))
+pilihan = float(input("masukkan pilihan (1/2/3):"))
 
-# konversi ke pounds(lb)
-if pilihan == "1" :
-    nilai = kg * 2.20462
-    print(f"{kg} kilogram sama dengan {nilai} pounds(lb)")
-# konversi ke ounce(ons)
-elif pilihan == "2" :
-    nilai = kg * 35.274
-    print(f"{kg} kilogram sama dengan {nilai} ounce(ons)")
-# konversi ke gram(g)
-elif pilihan == "3" :
-    nilai = kg * 1000
-    print(f"{kg} kilogram sama dengan {nilai} gram(g)")
-# jika pilihan bukan 1/2/3
+
+# pounds
+if pilihan == 1:
+    hasil = kilogram * 2.20462
+    print(kilogram,"*",2.20462,"=",hasil )
+
+# ons
+elif pilihan == 2:
+    hasil = kilogram * 35.274
+    print(kilogram,"*",35.274,"=",hasil )
+
+# gram
+elif pilihan == 3:
+    hasil = kilogram * 1000
+    print(kilogram,"*",1000,"=",hasil )
+
 else:
     print("pilihan tidak valid masukkan pilihan yang benar. Pilih 1, 2, atau 3")
+
